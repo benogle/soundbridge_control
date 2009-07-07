47882982ba6fe3d15660d28f64ac716bb8c9c2ae
Ben Ogle <benogle@ben-ogles-macbook.local>
2009-07-07 15:03:58 -0700

    more packaging mistakes

--- musicrollcontrol/urls.py
+++ musicrollcontrol/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^$', 'rokucontrol.musicrollcontrol.views.index', name="index"),
+    url(r'^$', 'soundbridge_control.musicrollcontrol.views.index', name="index"),
     
-    url(r'^filtersongs$', 'rokucontrol.musicrollcontrol.views.filtersongs', name="filtersongs"),
-    url(r'^serverconnect$', 'rokucontrol.musicrollcontrol.views.serverconnect', name="serverconnect"),
-    url(r'^filteraction$', 'rokucontrol.musicrollcontrol.views.filteraction', name="filteraction"),
+    url(r'^filtersongs$', 'soundbridge_control.musicrollcontrol.views.filtersongs', name="filtersongs"),
+    url(r'^serverconnect$', 'soundbridge_control.musicrollcontrol.views.serverconnect', name="serverconnect"),
+    url(r'^filteraction$', 'soundbridge_control.musicrollcontrol.views.filteraction', name="filteraction"),
     
-    url(r'^getplaylist$', 'rokucontrol.musicrollcontrol.views.getplaylist', name="getplaylist"),
+    url(r'^getplaylist$', 'soundbridge_control.musicrollcontrol.views.getplaylist', name="getplaylist"),
     
-    url(r'^playlistaction$', 'rokucontrol.musicrollcontrol.views.playlistaction', name="playlistaction"),
+    url(r'^playlistaction$', 'soundbridge_control.musicrollcontrol.views.playlistaction', name="playlistaction"),
     
-    url(r'^volume$', 'rokucontrol.musicrollcontrol.views.volume', name="volume"),
+    url(r'^volume$', 'soundbridge_control.musicrollcontrol.views.volume', name="volume"),
 )
